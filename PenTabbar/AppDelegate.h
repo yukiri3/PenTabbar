10eb500c91edb6dbd88e8022f9e50b4144e473fd
Li Chen Liou <game412214@gmail.com>
2017-02-03 15:41:53 +0800

    Initial Commit

---
+++ PenTabbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PenTabbar
+//
+//  Created by user on 2017/2/3.
+//  Copyright © 2017年 HeartCoding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PenTabbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PenTabbar
+//
+//  Created by user on 2017/2/3.
+//  Copyright © 2017年 HeartCoding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
